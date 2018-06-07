4dbaf353be496df3132f83559589df129ca7b3ab
Azamat Bekmuratov and Jim Palowski <azamat+jim@>
2018-06-07 11:27:17 -0700

    add child components

--- src/app/task-list/task-list.component.ts
+++ src/app/task-list/task-list.component.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Task } from '../models/task.model';
 
 @Component({
   selector: 'app-task-list',
   templateUrl: './task-list.component.html',
   styleUrls: ['./task-list.component.css']
 })
-export class TaskListComponent implements OnInit {
+export class TaskListComponent  {
+  @Input() childTaskList: Task[];
+  @Output() clickSender = new EventEmitter();
 
-  constructor() { }
+  filterByCompleteness: string = "incompleteTasks";
 
-  ngOnInit() {
+  editButtonClicked(taskToEdit: Task) {
+    this.clickSender.emit(taskToEdit);
   }
+
+  priorityColor(currentTask){
+    if (currentTask.priority === 3){
+      return "bg-danger";
+    } else if (currentTask.priority === 2) {
+      return  "bg-warning";
+    } else {
+      return "bg-info";
+    }
+  }
+
+  onChange(optionFromMenu) {
+    this.filterByCompleteness = optionFromMenu;
+  }
+
+  toggleDone(clickedTask: Task, setCompleteness: boolean) {
+    clickedTask.done = setCompleteness;
+  }
+
 
 }
